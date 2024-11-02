d77f1bc5b218af63180b3e8dd500bd4a3e0e6891
pavElle <sandra.wabokapche@gmail.com>
2024-11-02 06:24:17 +0100

    MainScreens

---
+++ screens/Events.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EventScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Event</Text>
+    </View>
+  )
+}
+
+export default EventScreen
+
+const styles = StyleSheet.create({})
